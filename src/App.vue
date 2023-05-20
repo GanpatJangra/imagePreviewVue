43cb15e105a22ac857dcac709668fda93d42f096
ganpatjangra <ganpatjangra@gmail.com>
2023-05-20 22:31:54 +0530

    update loading="lazy"

--- src/App.vue
+++ src/App.vue
@@ -25,7 +25,7 @@
             <input ref="openModelViewImageRef" type="checkbox" id="my-modal-200" class="modal-toggle" />
             <div class="modal modal-bottom  sm:modal-middle">
                 <div class="modal-box">
-                    <img lazy id="myImage" :src="imageUrl" alt="">
+                    <img loading="lazy" id="myImage" :src="imageUrl" alt="">
                     <div class="modal-action justify-between">
                         <label for="my-modal-200" class="btn btn-error btn-sm">Close !</label>
                         <button type="button" @click="downloadImage(imageUrl)" class="btn btn-success btn-sm">download
@@ -38,12 +38,12 @@
             </div>
         </div>
         <div class="bg-[#4406CB] w-full h-64">
-            <img lazy class="rounded-md h-40 mx-auto pt-14" src="@/assets/image/LogoICAI.png" alt="logo">
+            <img loading="lazy" class="rounded-md h-40 mx-auto pt-14" src="@/assets/image/LogoICAI.png" alt="logo">
         </div>
         <div class="-mt-20 md:w-2/3 w-[90%]  bg-white rounded mx-auto ">
             <div class="flex flex-col md:flex-row p-5">
                 <div class="md:w-1/2 w-full">
-                    <img lazy class="  h-full " src="@/assets/image/Image_to_show.jpg" alt="">
+                    <img loading="lazy" class="  h-full " src="@/assets/image/Image_to_show.jpg" alt="">
                 </div>
                 <div class="md:w-1/2 w-full md:px-10">
                     <div>
