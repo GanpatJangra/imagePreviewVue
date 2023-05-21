34d6e0c25564a50169c3206960ac2f7971d892c2
ganpatjangra <ganpatjangra@gmail.com>
2023-05-21 22:05:23 +0530

    add loader image generating

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
     <div class="bg-gray-300 pb-10">
         <div>
+            <!-- {{  formData.cropProfilePicture }} -->
+            <!-- <img class="h-32 w-32 " :src="formData.cropProfilePicture" alt=""> -->
             <input ref="openModelRef" type="checkbox" id="my-modal-6" class="modal-toggle" />
             <div class="modal modal-bottom sm:modal-middle">
                 <div class="modal-box">
-                    <vue-cropper ref="cropper" class="!h-[400px]" :aspect-ratio="16 / 16" :scalable="false" :cropBoxResizable="false"
-                        :src="formData.profile_image" dragMode="none" preview=".preview" />
+                    <vue-cropper ref="cropper" class="!h-[400px]" :aspect-ratio="16 / 16" :scalable="false"
+                        :cropBoxResizable="false" :src="formData.profile_image" dragMode="none" preview=".preview" />
                     <div>
                         <input @change="handelImageRangeValue" v-model.number="imageRangeValue" type="range" min="-0.5"
                             max="0.5" step="0.1" class="w-full mt-5 bg-gray-300 rounded-lg focus:outline-none" />
@@ -25,14 +27,37 @@
             <input ref="openModelViewImageRef" type="checkbox" id="my-modal-200" class="modal-toggle" />
             <div class="modal modal-bottom  sm:modal-middle">
                 <div class="modal-box">
-                    <img loading="lazy" id="myImage" :src="imageUrl" alt="">
-                    <div class="modal-action justify-between">
-                        <label for="my-modal-200" class="btn btn-error btn-sm">Close !</label>
-                        <button type="button" @click="downloadImage(imageUrl)" class="btn btn-success btn-sm">download
-                            Image</button>
-                        <!-- <div class="form_group_new">
-                                            <a class="download-btn" download="Certificate" href="https://dev.urja360.com/icai/uploads/64688d024a552.png">Download Certificate</a>
-                                        </div> -->
+                    <div v-if="imageUrl">
+                        <img loading="lazy" id="myImage" :src="imageUrl" alt="">
+                        <div class="modal-action justify-between">
+                            <label for="my-modal-200" class="btn btn-error btn-sm">Close !</label>
+                            <button type="button" @click="downloadImage(imageUrl)" class="btn btn-success btn-sm">download
+                                Image</button>
+                        </div>
+                    </div>
+                    <div v-else>
+                        <!-- <img loading="lazy" id="myImage" :src="imageUrl" alt=""> -->
+                        <div class="h-32 flex items-center flex-col capitalize font-medium justify-center ">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
+                                <g>
+                                    <circle cx="3" cy="12" r="2" fill="currentColor" />
+                                    <circle cx="21" cy="12" r="2" fill="currentColor" />
+                                    <circle cx="12" cy="21" r="2" fill="currentColor" />
+                                    <circle cx="12" cy="3" r="2" fill="currentColor" />
+                                    <circle cx="5.64" cy="5.64" r="2" fill="currentColor" />
+                                    <circle cx="18.36" cy="18.36" r="2" fill="currentColor" />
+                                    <circle cx="5.64" cy="18.36" r="2" fill="currentColor" />
+                                    <circle cx="18.36" cy="5.64" r="2" fill="currentColor" />
+                                    <animateTransform attributeName="transform" dur="1.5s" repeatCount="indefinite"
+                                        type="rotate" values="0 12 12;360 12 12" />
+                                </g>
+                            </svg>
+                            <h1>please wait image is generating ... </h1>
+                        </div>
+                        <div class="modal-action justify-between">
+                            <label for="my-modal-200" class="btn btn-error btn-sm">Close !</label>
+                            <button type="button" class="btn btn-success btn-sm">Wait ....</button>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -173,7 +198,7 @@
 import { useHead } from "@vueuse/head"
 import VueCropper from 'vue-cropperjs';
 import 'cropperjs/dist/cropper.css';
-import { ref, computed,reactive, watch } from 'vue';
+import { ref, computed, reactive, watch } from 'vue';
 import axios from 'axios';
 const formData = ref({
     profile_image: null,
@@ -196,7 +221,7 @@ function cropImage() {
     openModelRef.value.click()
 }
 function handelImageRangeValue() {
-    console.log("🚀 ~ file: app.vue:199 ~ handelImageRangeValue ~ imageRangeValue.value:", imageRangeValue.value)
+    // console.log("🚀 ~ file: app.vue:199 ~ handelImageRangeValue ~ imageRangeValue.value:", imageRangeValue.value)
     cropper.value.relativeZoom(imageRangeValue.value);
     // zoom(percent) {
     //     this.$refs.cropper.relativeZoom(percent);
@@ -253,11 +278,13 @@ async function submitForm() {
     if (formData.value.designation.length > 20) {
         return errors.value.push('Designation should not exceed 20 characters');
     }
+    openModelViewImageRef.value.click()
+
     let form = new FormData();
     const imageData = await formData.value.cropProfilePicture; // Replace with your actual Base64-encoded image data
     enableLoading.value = true
     // Create a Blob from the Base64 data
-    const blob = await base64ToBlob(imageData);
+    const blob = await base64ToBlob(imageData)
     // Append the image blob to the form data
     form.append('name', formData.value.name);
     form.append('designation', formData.value.designation);
@@ -271,7 +298,6 @@ async function submitForm() {
     axios.post('https://apifincobox.sentientdigital.in/api/v1/gib/image', form, { headers })
         .then(resp => {
             imageUrl.value = resp.data.results.image_url;
-            openModelViewImageRef.value.click()
             enableLoading.value = false
             // downloadImage()
         })
@@ -330,21 +356,21 @@ async function downloadImage(imageURL) {
 }
 
 const siteData = reactive({
-      title: `The Institute of Chartered Accountants of India `,
-      description: `The Institute of Chartered Accountants of India, Committee on MSME & Startup, ICAI`,
-    })
+    title: `The Institute of Chartered Accountants of India `,
+    description: `The Institute of Chartered Accountants of India, Committee on MSME & Startup, ICAI`,
+})
 
-    useHead({
-      // Can be static or computed
-      title: () => siteData.title,
-      meta: [
+useHead({
+    // Can be static or computed
+    title: () => siteData.title,
+    meta: [
         {
-          name: `description`,
-          content: () => siteData.description,
-          
+            name: `description`,
+            content: () => siteData.description,
+
         },
-      ],
-    })
+    ],
+})
 console.log('v12')
 </script>
 <style>
